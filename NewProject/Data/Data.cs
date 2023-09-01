d0c64d1c53973311294bd9b8d0effef7872f632f
sergeymoshtakov <113889721+sergeymoshtakov@users.noreply.github.com>
2023-09-01 20:46:27 +0200

    Add files via upload

---
+++ NewProject/Data/Data.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewProject.Data
+{
+    public class Data : DbContext
+    {
+        public DbSet<Entity.Department> Departments { get; set; }
+        public DbSet<Entity.Manager> Managers { get; set; }
+        public Data() : base()
+        { 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=NewProject;Integrated Security=True"
+                );
+        }
+    }
+}
